7282cc4c516c9591c9e531761be0ae5863439d8e
Gallay Lajos <gallay.lajos@gmail.com>
2024-06-20 17:23:10 +0200

    background color fix

--- frontend/src/components/layout.tsx
+++ frontend/src/components/layout.tsx
@@ -21,9 +21,8 @@ export const Layout = Shade({
           overflow: 'hidden',
           padding: '0',
           margin: '0',
-          backgroundColor: injector.getInstance(ThemeProviderService).theme.background.paper,
-        }}
-      >
+          background: injector.getInstance(ThemeProviderService).theme.background.default,
+        }}>
         <Header title="JSON-Tools" />
         <Body style={{ width: '100%', height: '100%', overflow: 'auto' }} />
       </div>
